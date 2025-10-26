81d282cb3d2d6389061b73a58ff5ae9d48745a56
mikub97 <mikub97@gmail.com>
2025-10-26 12:28:21 +0100

    ifs and scenarios

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+name = input("Whats your name?\n")
+print("Welcome", name, "!")
+age = int(input("How old are you?\n"))
+
+if age>=18:
+    print("You are an adult,",name)
+    ans = input("Would you like a drink? (y/n)")
+    if ans.lower() == "n":  # 'n'->'n'  'N'->'n'
+        print("bye bye")
+    elif ans.lower() == "y":
+        print("here you go..")
+    else:
+        print("im not sure what you mean..")
+else:
+    print("You are not adult",name)
+    print("byebye")
